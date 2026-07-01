d2206a5a6732e5486c907ebdcb93ac62fd304d95
Simrankhan82 <simranmehruddinkhan@gmail.com>
2026-07-01 13:59:11 +0530

    Time: 2 ms (64.22%), Space: 46.3 MB (62.92%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,38 +15,36 @@
  */
 class Solution { 
 
-    int preIndex = 0;
-    Map<Integer, Integer> map = new HashMap<>();
+    int preIndex = 0 ; 
+    HashMap<Integer , Integer> map = new HashMap<>();
 
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-
-        // Store inorder value -> index
-        for (int i = 0; i < inorder.length; i++) {
-            map.put(inorder[i], i);
+    public TreeNode buildTree(int[] preorder , int[]inorder){
+        for(int i =0 ; i < inorder.length ; i++){
+            map.put(inorder[i] , i);
         }
 
-        return build(preorder, 0, inorder.length - 1);
+        return build(preorder , 0 , inorder.length-1);
+
     }
 
-    private TreeNode build(int[] preorder, int left, int right) {
-
-        if (left > right) {
-            return null;
+    private TreeNode build(int[]preorder , int left , int right){
+        if(left > right){
+            return null ;
         }
 
-        // Root comes from preorder
         int rootValue = preorder[preIndex++];
         TreeNode root = new TreeNode(rootValue);
 
-        // Find root position in inorder
+
         int mid = map.get(rootValue);
 
-        // Build left subtree
-        root.left = build(preorder, left, mid - 1);
-
-        // Build right subtree
-        root.right = build(preorder, mid + 1, right);
+        root.left = build(preorder , left , mid-1);
+        root.right = build(preorder ,mid+1 , right);
 
         return root;
+
     }
 }
+
+
+
