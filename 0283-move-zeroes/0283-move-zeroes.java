bd6a1ffc2c7f952de5ab838044ffaf6e8ead1334
Simrankhan82 <simranmehruddinkhan@gmail.com>
2026-07-06 11:17:59 +0530

    Time: 1 ms (99.84%), Space: 47.5 MB (87.94%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+       int  InsertPosition = 0 ;
+
+        for(int i = 0 ; i < nums.length ; i++){
+
+            if(nums[i] != 0){
+                nums[InsertPosition] = nums[i];
+                InsertPosition++;
+
+            }
+        }
+            while(InsertPosition < nums.length){
+                nums[InsertPosition++] = 0 ;
+            
+        }
+    }
+}
