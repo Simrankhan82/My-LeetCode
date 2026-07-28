a425fe3261304180d6f4598e342f9c12bc13f8ab
Simrankhan82 <simranmehruddinkhan@gmail.com>
2026-07-28 17:05:46 +0530

    Time: 4 ms (73.87%), Space: 45.3 MB (36.73%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -3,41 +3,26 @@ import java.util.*;
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-        // Stack to maintain a monotonic decreasing order
         Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        // Stores next greater element for each index of nums2
-        int[] nextGreater = new int[nums2.length];
-
-        // Step 1: Find next greater element for every element in nums2
+        // Find the next greater element for every value in nums2
         for (int i = nums2.length - 1; i >= 0; i--) {
 
-            int element = nums2[i];
-
-            // Remove all smaller or equal elements
-            while (!stack.isEmpty() && stack.peek() <= element) {
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
                 stack.pop();
             }
 
-            // Store the answer
             if (stack.isEmpty()) {
-                nextGreater[i] = -1;
+                map.put(nums2[i], -1);
             } else {
-                nextGreater[i] = stack.peek();
+                map.put(nums2[i], stack.peek());
             }
 
-            // Push current element
-            stack.push(element);
+            stack.push(nums2[i]);
         }
 
-        // Step 2: Store (value -> next greater element)
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums2.length; i++) {
-            map.put(nums2[i], nextGreater[i]);
-        }
-
-        // Step 3: Build the answer for nums1
+        // Build the answer for nums1
         int[] ans = new int[nums1.length];
 
         for (int i = 0; i < nums1.length; i++) {
